74e834eedb59846a83ed31f29211e4e58801758c
Du.Kang <banshee1115@163.com>
2024-05-06 16:56:04 +0800

    插槽组件构建公共title

--- wallpaper/pages/index/index.vue
+++ wallpaper/pages/index/index.vue
@@ -23,7 +23,36 @@
 				<view class="right">
 					<uni-icons type="forward" size="20" color="#28b389"></uni-icons>
 				</view>
-			
+		</view>
+		<!-- 公共标题 -->
+		<view class="select">
+			<common-title>
+				<template #name>每日推荐</template>
+				<template #custom>
+					<view class="date">
+						<uni-icons type="calendar" size="18" color="#28b389"></uni-icons>
+						<view class="text">
+							<uni-dateformat format="dd" :date="Date.now()"></uni-dateformat>日
+						</view>
+					</view>
+				</template>
+			</common-title>
+			<view class="content">
+					<scroll-view scroll-x >
+						<view class="box" v-for="item in 8" :key="item">
+							<image src="../../common/images/col.jpeg" mode="aspectFill"></image>
+						</view>
+					</scroll-view>
+			</view>
+		</view>
+		<!-- 专题 -->
+		<view class="theme">
+			<common-title>
+				<template #name>专题精选</template>
+				<template #custom>
+					<navigator url="" class="more">更多...</navigator>
+				</template>
+			</common-title>
 		</view>
 	</view>
 </template>
@@ -89,6 +118,47 @@
 				display: flex;
 				justify-content: center;
 				align-items: center;
+			}
+		}
+		.select {
+			padding-top: 50rpx;
+			.date {
+				display: flex;
+				align-items: center;
+				color: #28b389;
+				.text {
+					margin-left: 5rpx;
+				}
+			}
+			.content {
+				width: 725rpx;
+				margin-left: 25rpx;
+				margin-top: 25rpx;
+				scroll-view {
+					white-space: nowrap;
+					.box {
+						width: 200rpx;
+						height: 430rpx;
+						display: inline-block;
+						margin-right: 15rpx;
+						image {
+							width: 100%;
+							height: 100%;
+							border-radius: 25rpx;
+						}
+					}
+					.box:last-child {
+						margin-right: 25rpx;
+					}
+				}
+			}
+		}
+		
+		.theme {
+			padding-top: 50rpx;
+			.more{
+				font-size: 32rpx;
+				color: #888;
 			}
 		}
 	} 
